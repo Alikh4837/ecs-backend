bae91173079c576fde4168a666a2b6e020a0348a
Ali Haider <Alikh4837@gmail.com>
2026-03-04 10:22:59 +0500

    Initial commit / Django project ready for deployment

--- backend/settings.py
+++ backend/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('helloeveryonewelcometomybackenddjangoproject12345')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = ['AliHaider1.pythonanywhere.com']
 
